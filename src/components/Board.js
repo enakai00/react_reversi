78e2b7c49717e3390e3d1bfdc02392fe5905ff8c
Etsuji Nakai <enakai0@gmail.com>
2023-03-07 00:45:02 +0000

    Update

--- src/components/Board.js
+++ src/components/Board.js
@@ -12,22 +12,28 @@ const Cell = (props) => {
   let x = 25 + 70 * props.pos_x;
   let y = 13 + 70 * props.pos_y;
   const style = {
-	  position: "absolute", left: x, top: y,
-          width: "67px", height: "67px"
+    position: "absolute",
+    width: "67px", height: "67px",
+    left: x, top: y,
   };
-  let element = (
-    <img src={blank} alt="blank"
-	  onClick={props.onClick} style={style}/>
-  );
-  if (props.mark === "b") {
-    element = (
-      <img src={black} alt="black" style={style}/>
-    );
-  }
-  if (props.mark === "w") {
-    element = (
-      <img src={white} alt="white" style={style}/>
-    );
+
+  var element;
+  switch (props.mark) {
+    case "b":
+      element = (
+        <img src={black} alt="black" style={style}/>
+      );
+    break;
+    case "w":
+      element = (
+        <img src={white} alt="white" style={style}/>
+      );
+    break;
+    default:
+      element = (
+        <img src={blank} alt="blank" style={style}
+             onClick={props.onClick}/>
+      );
   }
   return element;
 };
@@ -52,6 +58,7 @@ class BoardInfo {
 
 
 export const Board = (props) => {
+  const opponent = {"b": "w", "w": "b"}
   const boardInfo = useRef(new BoardInfo(props));
   const info = boardInfo.current;
   // Since `field` stores an array object, updating it doesn't rerender the component.
@@ -59,61 +66,69 @@ export const Board = (props) => {
   // eslint-disable-next-line
   const [dummyState, setDummyState] = useState([]);
 
+  const sleep = (milliseconds) => {
+    return new Promise(resolve => setTimeout(resolve, milliseconds))
+  };
+
   const pass = () => {
-    var opponent;
-    if (info.turn === "b") {
-      opponent = "w";
-    } else {
-      opponent = "b";
-    }
-    info.turn = opponent;
+    info.turn = opponent[info.turn];
     setDummyState([]);
   };
 
   const onClick = (x, y) => {
+    move(x, y);
+  };
+
+  const move = async (x, y) => {
+    // 8 directions to search.
     const dx = [1,-1, 0, 0,-1, 1, 1,-1]
     const dy = [0, 0, 1,-1,-1,-1, 1, 1]
-    var opponent;
-    if (info.turn === "b") {
-      opponent = "w";
-    } else {
-      opponent = "b";
-    }
 
     let allowed = false;
     for (let i = 0; i < 8; i++) { // search for 8 directions.
-      if (info.field[y][x] !== " ") {
-	break;
+      if (info.field[y][x] !== " ") { // not a blank cell
+        break;
       }
       let search_state = 0;
       var j;
       for (j = 1; j < 8; j++) {
-	const xx = x + j*dx[i];
-	const yy = y + j*dy[i];
+        const xx = x + j*dx[i];
+        const yy = y + j*dy[i];
         if (xx < 0 || xx >= info.size || yy < 0 || yy >= info.size) {
           break; // search failed.
         }
-        if (info.field[yy][xx] === opponent) {
-	  search_state = 1;
-	  continue;
+        if (info.field[yy][xx] === opponent[info.turn]) {
+          search_state = 1;
+          continue;
         }
         if (search_state === 1 && info.field[yy][xx] === info.turn) {
-	  search_state = 2; // search succeeded.
-	  break;
+          search_state = 2; // search succeeded.
+          break;
         }
-	break; // search failed.
+        break; // search failed.
       }
+
       if (search_state === 2) {
-	allowed = true;
-        for (let jj = 1; jj < j; jj++) {
-	  info.field[y + jj*dy[i]][x + jj*dx[i]] = info.turn;
+        // Animation starts.
+        info.field[y][x] = info.turn;
+        if (!allowed) {
+          await sleep(100);
+          await setDummyState([]);
         }
+        for (let jj = 1; jj < j; jj++) {
+          info.field[y + jj*dy[i]][x + jj*dx[i]] = info.turn;
+          await sleep(100);
+          await setDummyState([]);
+        }
+        info.field[y][x] = " ";
+        allowed = true;
+        // Animation ends.
       }
     }
     if (allowed) {
       info.field[y][x] = info.turn;
-      info.turn = opponent;
-      setDummyState([]);
+      info.turn = opponent[info.turn];
+      await setDummyState([]);
     }
   };
 
@@ -123,50 +138,43 @@ export const Board = (props) => {
   for (let y = 0; y < info.size; y++) {
     for (let x = 0; x < info.size; x++) {
       if (info.field[y][x] === "b"){
-	black_score += 1;
+        black_score += 1;
       }
       if (info.field[y][x] === "w"){
-	white_score += 1;
+        white_score += 1;
       }
       // Add the cell value to the unique key so that modified cell will be rerendered.
       field_elements.push(
         <Cell key={y.toString()+x.toString()+info.field[y][x]}
-	      pos_x={x} pos_y={y} mark={info.field[y][x]}
-	      onClick={() => onClick(x, y)} />
+              pos_x={x} pos_y={y} mark={info.field[y][x]}
+              onClick={() => onClick(x, y)} />
       );
     }
-  };
-
-  var info_elements;
-  const style = {
-    width: "24px", height: "24px", float: "left"
-  };
-  if (info.turn === "b") {
-    info_elements = (
-      <div>
-        <img src={black} alt="black" style={style}/> {black_score}pieces. : Your turn
-        <br/>
-        <img src={white} alt="white" style={style}/> {white_score}pieces.
-      </div>
-    );
-  } else {
-    info_elements = (
-      <div>
-        <img src={black} alt="black" style={style}/> {black_score}pieces.
-        <br/>
-        <img src={white} alt="white" style={style}/> {white_score}pieces. : Your turn
-      </div>
-    );
   }
 
+  var info_elements;
+  const style = {width: "24px", height: "24px", float: "left"};
+  const black_turn = {"b": ": Your turn", "w": ""};
+  const white_turn = {"w": ": Your turn", "b": ""};
+  info_elements = (
+    <Stack spacing="2">
+    <Box>
+      <img src={black} alt="black" style={style}/> {black_score}pieces. {black_turn[info.turn]}
+    </Box>
+    <Box>
+      <img src={white} alt="white" style={style}/> {white_score}pieces. {white_turn[info.turn]}
+    </Box>
+    </Stack>
+  );
+
   const element = (
-    <Stack spacing="1">
-      <div className="bg-image">
+    <Stack spacing="2">
+      <Box className="bg-image">
         {field_elements}
-      </div>
-      <div style={{marginLeft: 40, marginTop: 20, marginBottom: 10}}>
+      </Box>
+      <Box style={{marginLeft: 40, marginTop: 20, marginBottom: 10}}>
         {info_elements}
-      </div>
+      </Box>
       <Box><Button colorScheme="red" size="sm" onClick={pass}>Pass</Button></Box>
     </Stack>
   );
--- src/components/Layout.js
+++ src/components/Layout.js
@@ -12,18 +12,16 @@ export const Layout = (props) => {
   };
 
   const element = (
-      <Container p="8" minWidth="2xl" maxWidth="2xl">
-        <Stack spacing="1">
-          <Heading m="2" as="h1" fontSize="3xl" w="full">Reversi</Heading>
-        </Stack>
-
+    <Container p="8" minWidth="2xl" maxWidth="2xl">
+      <Stack spacing="1">
+        <Heading m="2" as="h1" fontSize="3xl" w="full">Reversi</Heading>
         <Board key={gameID}/>
-        <br/>
-        <Stack spacing="1">
-          <Box><Button colorScheme="blue" size="sm"
-            onClick={restart}>Restart</Button></Box>
-        </Stack>
-      </Container>
+        <Box style={{marginTop: 10, marginBottom: 10}}>
+          <Button colorScheme="blue" size="sm"
+                  onClick={restart}>Restart</Button>
+        </Box>
+      </Stack>
+    </Container>
   );
 
   return element;
