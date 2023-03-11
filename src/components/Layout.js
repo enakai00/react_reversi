e9774eb3d0707b8e0f3080354974a79ab471e857
Etsuji Nakai <enakai0@gmail.com>
2023-03-11 10:55:17 +0000

    Update

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,16 +1,34 @@
-import React from "react";
-import { Container, Stack, Heading, Box } from "@chakra-ui/react";
+import React, { useState } from "react";
+import { Container, Stack,
+         Heading, Box, HStack, Button } from "@chakra-ui/react";
 
 import { Board } from "./Board";
 
 
 export const Layout = (props) => {
+  const [gameID, setGameID] = useState(new Date().getTime());
+
+  const restart = () => {
+    setGameID(new Date().getTime());
+  }
+
+  const pass = () => {
+  }
+
   const element = (
     <Container padding="8">
       <Stack spacing="1">
         <Heading marginLeft="4" fontSize="3xl">Reversi</Heading>
         <Box>
-          <Board/>
+          <Board key={gameID} />
+        </Box>
+        <Box>
+          <HStack>
+            <Button colorScheme="red" size="sm"
+                    onClick={pass}>Pass</Button>
+            <Button colorScheme="blue" size="sm"
+                    onClick={restart}>Restart</Button>
+          </HStack>
         </Box>
       </Stack>
     </Container>
